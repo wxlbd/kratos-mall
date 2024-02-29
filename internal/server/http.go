1c6f1cc2cfa43193bc59debc64a7f7f692634784
wxl <297750182@qq.com>
2024-02-29 17:20:21 +0800

    feat: 中间件和路由白名单

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,20 +1,34 @@
 package server
 
 import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/metadata"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/http"
 	v1 "kratos-admin/api/helloworld/v1"
+	"kratos-admin/internal/biz"
 	"kratos-admin/internal/conf"
 	"kratos-admin/internal/service"
-
-	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger, au *biz.AuthUseCase) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
-			recovery.Recovery(),
+			recovery.Recovery(),    //异常恢复
+			tracing.Server(),       //链路追踪中间件
+			logging.Server(logger), //日志中间件
+			metrics.Server(),       //监控中间件
+			validate.Validator(),   //参数校验
+			metadata.Server(),      //元数据中间
+			selector.Server(au.JwtMiddleware()).
+				Match(NewWhiteListMatcher()).Build(), //路由选择中间件
 		),
 	}
 	if c.Http.Network != "" {
@@ -29,4 +43,17 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	srv := http.NewServer(opts...)
 	v1.RegisterGreeterHTTPServer(srv, greeter)
 	return srv
+}
+
+// NewWhiteListMatcher 路由白名单
+func NewWhiteListMatcher() selector.MatchFunc {
+	whiteList := make(map[string]struct{})
+	whiteList["/shop.interface.v1.ShopInterface/Login"] = struct{}{}
+	whiteList["/shop.interface.v1.ShopInterface/Register"] = struct{}{}
+	return func(ctx context.Context, operation string) bool {
+		if _, ok := whiteList[operation]; ok {
+			return false
+		}
+		return true
+	}
 }
