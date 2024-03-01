8be333e8bb2eeb5d79a8873bf45bd4e7d73e0c88
wxl <297750182@qq.com>
2024-03-01 15:42:32 +0800

    feat: redis.UniversalClient、gorm.DB提供者

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,25 +1,65 @@
 package data
 
 import (
-	"kratos-admin/internal/conf"
-
+	"context"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
 	redis "github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
+	_ "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"kratos-admin/internal/conf"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewGreeterRepo, NewGorm, NewRedis)
 
 // Data .
 type Data struct {
 	redis.UniversalClient
+	*gorm.DB
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB, rdb redis.UniversalClient) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{
+		UniversalClient: rdb,
+		DB:              db,
+	}, cleanup, nil
+}
+
+func NewGorm(cfg *conf.Data, logger log.Logger) (*gorm.DB, func()) {
+	db, err := gorm.Open(mysql.Open(cfg.Database.Source), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDB.SetConnMaxIdleTime(60)
+	sqlDB.SetMaxOpenConns(20)
+	f := func() {
+		_ = sqlDB.Close()
+	}
+	return db, f
+}
+
+func NewRedis(c *conf.Data) (redis.UniversalClient, error) {
+	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:        c.GetRedis().GetAddrs(),
+		DB:           int(c.GetRedis().Db),
+		Username:     c.GetRedis().Username,
+		Password:     c.GetRedis().Password,
+		ReadTimeout:  c.GetRedis().ReadTimeout.AsDuration(),
+		WriteTimeout: c.GetRedis().WriteTimeout.AsDuration(),
+	})
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		panic(err)
+		return nil, err
+	}
+	return rdb, nil
 }
