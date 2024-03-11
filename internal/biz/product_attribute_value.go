51db0caa79e9c9d0f95ce34e023deae5061798fb
wxl <297750182@qq.com>
2024-03-11 22:11:10 +0800

    feat: 商品属性值逻辑层实现

---
+++ internal/biz/product_attribute_value.go
@@ -0,0 +1,34 @@
+package biz
+
+import (
+	"context"
+	v1 "kratos-admin/api/product/v1"
+)
+
+type ProductAttributeValueUseCase struct {
+	productAttributeValueRepo ProductAttributeValueRepo
+}
+
+func NewProductAttributeValueUseCase(productAttributeValueRepo ProductAttributeValueRepo) *ProductAttributeValueUseCase {
+	return &ProductAttributeValueUseCase{
+		productAttributeValueRepo: productAttributeValueRepo,
+	}
+}
+
+func (u *ProductAttributeValueUseCase) CreateOrUpdateProductAttributeValue(ctx context.Context, param *v1.CreateOrUpdateProductAttributeValueRequest) error {
+	return u.productAttributeValueRepo.CreateOrUpdateProductAttributeValue(ctx, param)
+}
+
+func (u *ProductAttributeValueUseCase) DeleteProductAttributeValue(ctx context.Context, id int64) error {
+	return u.productAttributeValueRepo.DeleteProductAttributeValue(ctx, id)
+}
+
+func (u *ProductAttributeValueUseCase) FindProductAttributeValueByAttributeId(ctx context.Context, attributeId int64) (*v1.FindProductAttributeValueByAttributeIdReply, error) {
+	values, err := u.productAttributeValueRepo.FindProductAttributeValueByAttributeId(ctx, attributeId)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.FindProductAttributeValueByAttributeIdReply{
+		ProductAttributeValues: values,
+	}, nil
+}
