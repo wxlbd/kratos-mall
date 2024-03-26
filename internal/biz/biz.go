2c5d65aee02c3d19d919264e8bab0d88cb54b567
wxl <297750182@qq.com>
2024-03-26 22:01:58 +0800

    fix: gorm空日志输出问题修复

--- internal/biz/biz.go
+++
@@ -1,6 +0,0 @@
-package biz
-
-import "github.com/google/wire"
-
-// ProviderSet is biz providers.
-var ProviderSet = wire.NewSet(NewProductCategoryUseCase, NewProductUseCase, NewProductAttributeUseCase, NewProductAttributeValueUseCase, NewAuthUseCase)
